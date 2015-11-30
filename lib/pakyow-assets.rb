a9578c097e3dacc8b2234f6bfb7835c839ebddc3
Bryan Powell <bryan@metabahn.com>
2015-11-29 19:49:32 -0600

    Fix processor registration

--- lib/pakyow-assets.rb
+++ lib/pakyow-assets.rb
@@ -8,16 +8,20 @@ require_relative 'preprocessors/image-preprocessor'
 require_relative 'preprocessors/javascript-preprocessor'
 require_relative 'preprocessors/sass-preprocessor'
 
+Pakyow::App.before :init do
+  if config.assets.compile_on_startup
+    Pakyow::App.processor :html do |content|
+      Pakyow::Assets.mixin_fingerprints(content)
+    end
+  end
+end
+
 Pakyow::App.after :init do
   config.assets.stores.each_pair do |name, path|
     Pakyow::Assets.register_path_with_name(path, name)
   end
 
   if config.assets.compile_on_startup
-    Pakyow::App.processor :html do |content|
-      Pakyow::Assets.mixin_fingerprints(content)
-    end
-
     Pakyow.logger.debug 'Precompiling assets...'
     Pakyow::Assets.precompile
     Pakyow.logger.debug 'Finished precompiling!'
